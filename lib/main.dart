54d30d3998d810c89327a78a455fc3b2192898a5
michael <emikeovo@gmail.com>
2019-11-30 16:03:18 +0100

    Added IndieFlower fontfamily file and modified colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,23 @@ void main() => runApp(MaterialApp(
     appBar:AppBar(
       title:Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[900],
     ),
     body:Center(
-      child: Text('Hello prof!'),
+      child: Text('Hello prof!',
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color:Colors.grey[900],
+        fontFamily: 'IndieFlower',
+      ),),
 
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text('Click'),
+      backgroundColor: Colors.red[900],
     ),
   ),
 ));
