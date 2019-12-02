1ca608486ce0e94b711ac6a035c40b506d546b77
michael <emikeovo@gmail.com>
2019-12-02 19:22:18 +0100

    Added customized container

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,30 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
-        title:Text('my first app'),
+        title:Text('Techno-Y2'),
         centerTitle: true,
-        backgroundColor: Colors.red[900],
+        backgroundColor: Colors.blue[900],
       ),
-      body:Center(
-        child: IconButton(
-          onPressed: (){print('you click me');},
-          icon:Icon(Icons.alternate_email),
-          color: Colors.red[600],
-        ),
+      body:Container(
+        padding: EdgeInsets.all(15.0),
+        //symmetric(horizontal: 30.0, vertical: 12.0),
+        //fromLTRB(10.0, 10.0, 15.0, 10.0),
+        margin: EdgeInsets.all(10.0),
+        //fromLTRB(200.0, 50.0, 0.0, 100.0),
+       // symmetric(vertical: 100.0,horizontal: 100.0),
+       
+        color: Colors.red[200],
+       child: Text('Hello'),
+
+      ),
+//      Center(
+//        child:
+
+//        IconButton(
+//          onPressed: (){print('you click me');},
+//          icon:Icon(Icons.alternate_email),
+//          color: Colors.red[600],
+//        ),
 
 //        RaisedButton.icon(
 //          onPressed: (){},
@@ -30,14 +44,15 @@ class Home extends StatelessWidget {
 //          color: Colors.pink,
 //        ),
 
-                //Image.asset('assets/david-clode-x.jpg'),
-                //Image.network(''),
-        ),
+        //Image.asset('assets/david-clode-x.jpg'),
+        //Image.network(''),
+
+//        ),
 
     floatingActionButton: FloatingActionButton(
     onPressed: () {},
     child: Text('Click'),
-    backgroundColor: Colors.red[900],
+    backgroundColor: Colors.blue[900],
     ),
 
     );
